e22dff1c2163382b8c02c9bcf372dc5f6b8399bd
alakijaayo <alakijaayo@gmail.com>
2019-02-17 10:40:29 +0000

    Writing method and refactoring
    Writing a method that checks all the numbers and passes all the tests. It will return the necessary output for each number. Also, refactored my code to remove all methods and make code cleaner and easier to read

--- src/factor.js
+++ src/factor.js
@@ -2,19 +2,18 @@ function Factor() {
 
 }
 
-Factor.prototype.pling = function(n) {
-  n % 3 === 0
-  return "Pling"
-};
-
-Factor.prototype.plang = function(n) {
-  n & 5 === 0
-  return "Plang"
-};
-
-Factor.prototype.plong = function(n) {
-  n % 7 === 0
-  return "Plong"
+Factor.prototype.check_number = function(n) {
+  let output = "";
+  if (n % 3 === 0) {
+    output += "Pling";
+  }
+  if (n % 5 === 0) {
+    output += "Plang";
+  }
+  if (n % 7 === 0) {
+    output += "Plong";
+  }
+  return output === "" ? n.toString() : output;
 };
 
 module.exports = Factor
